934a19772096897890fa99543f3844b18fdb3752
Matt Carabine <matt.carabine@hotmail.co.uk>
2016-06-21 19:27:32 +0100

    Update server Docker to use persistent volumes

--- deployment/docker/server/run.sh
+++ deployment/docker/server/run.sh
@@ -1,1 +1,7 @@
-lnt runserver /lnt/db --hostname=0.0.0.0
+if lnt create /lnt/db/db ; then
+    echo "No existing database found, new one created.\n"
+else
+    echo "Existing database found, not creating new one. If you wish to create a new database please delete existing database on the host and re-run container.\n"
+fi
+
+lnt runserver /lnt/db/db --hostname=0.0.0.0

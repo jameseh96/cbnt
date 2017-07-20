090a0b846ca982e51559146f114021610e0fe4fc
Chris Matthews <cmatthews5@apple.com>
2017-07-20 00:09:11 +0000

    LNT API run roundtrips
    Allow LNT runs to be downloaded from the API in a format that they can
    be inserted back into the same or other LNT instances.  This can be used
    to move runs between LNT instances, or modify them.
    
    I make some changes to the REST API to make the format closer to what we
    expect, then ignore some fields (like ID) when importing.
    
    git-svn-id: https://llvm.org/svn/llvm-project/lnt/trunk@308565 91177308-0d34-0410-b5e6-96231b3b80d8

--- lnt/server/db/testsuitedb.py
+++ lnt/server/db/testsuitedb.py
@@ -1220,6 +1220,9 @@ class TestSuiteDB(object):
         # Ignore incoming ids; we will create our own
         run_parameters.pop('id', None)
 
+        # Added by REST API, we will replace as well.
+        run_parameters.pop('order_by', None)
+
         # Find the order record.
         order, inserted = self._getOrCreateOrder(run_parameters, cv=cv)
 

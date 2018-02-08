d4edb34f94c2c3f6ea6f975a8d8242a4e256f2be
Tim Bradgate <timbradgate@hotmail.co.uk>
2018-02-08 14:46:58 +0000

    Update replay script to include latest changes

--- examples/replay-history-new.sh
+++ examples/replay-history-new.sh
@@ -63,6 +63,11 @@ git fetch http://review.couchbase.org/kv_engine refs/changes/42/87842/5
 git fetch http://review.couchbase.org/kv_engine refs/changes/30/88030/3
 git fetch http://review.couchbase.org/kv_engine refs/changes/09/87909/16
 git fetch http://review.couchbase.org/kv_engine refs/changes/78/87878/8
+git fetch http://review.couchbase.org/kv_engine refs/changes/64/88564/8
+git fetch http://review.couchbase.org/kv_engine refs/changes/68/88568/6
+git fetch http://review.couchbase.org/kv_engine refs/changes/64/88764/4
+git fetch http://review.couchbase.org/kv_engine refs/changes/64/88864/4
+
 if [ -z ${START_COMMIT+x} ]; then
     echo "Checking out $START_COMMIT"
     git checkout $START_COMMIT
@@ -93,9 +98,22 @@ for OUTPUT in $GIT_LOG; do
     if ! git merge-base --is-ancestor a41634456d8ca0e28aedec8ee5306f2c7531518c HEAD 2> /dev/null;
         then git cherry-pick -n a41634456d8ca0e28aedec8ee5306f2c7531518c;
     fi
+    if ! git merge-base --is-ancestor 3547c1d03cd4c0ae1ec2a8dd3c99222eb89e0020 HEAD 2> /dev/null;
+        then git cherry-pick -n 3547c1d03cd4c0ae1ec2a8dd3c99222eb89e0020;
+    fi
+    if ! git merge-base --is-ancestor 1f2ec746fad99952489636c6af9f1aded1f0ab12 HEAD 2> /dev/null;
+        then git cherry-pick -n 1f2ec746fad99952489636c6af9f1aded1f0ab12;
+    fi
+    if ! git merge-base --is-ancestor e493d071e2cf9259247bb48f0961d5929a5c2d8e HEAD 2> /dev/null;
+        then git cherry-pick -n e493d071e2cf9259247bb48f0961d5929a5c2d8e;
+    fi
+    if ! git merge-base --is-ancestor 0f3bfd2532c546442608447b0e9326dcbaf7a698 HEAD 2> /dev/null;
+        then git cherry-pick -n 0f3bfd2532c546442608447b0e9326dcbaf7a698;
+    fi
     cd ..
     make -j24
-    CBNT_MACHINE_NAME="KV-Engine-Perf-2" lnt runtest "kv-engine" kv_engine/tests/cbnt_tests/cbnt_test_list.yml master --submit_url=http://172.23.122.48/submitRun -v --commit=1 --iterations=5
+    export CBNT_MACHINE_NAME="KV-Engine-Perf-2"
+    numactl --interleave=all lnt runtest "kv-engine" kv_engine/tests/cbnt_tests/cbnt_test_list.yml master --submit_url=http://172.23.122.48/submitRun -v --commit=1 --iterations=5
     make clean -j24
 
     if [ ${TIMEOUT+x} ]; then

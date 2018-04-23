llvm-mirror/lnt

Avoid lazy queries in recent page
We were using lazy queries to get Order and machine info. Those are
always rendered, so fetch them unconditionally in the first query.

git-svn-id: https://llvm.org/svn/llvm-project/lnt/trunk@305724 91177308-0d34-0410-b5e6-96231b3b80d8

lnt/server/ui/views.py
@@ -18,6 +18,7 @@ from flask import render_template
 from flask import request, url_for
 from flask import session
 from flask_wtf import Form
+from sqlalchemy.orm import joinedload
 from sqlalchemy.orm.exc import NoResultFound
 from typing import List, Optional
 from wtforms import SelectField, StringField, SubmitField
@@ -176,9 +177,13 @@ def v4_recent_activity():
 
     # Get the most recent runs in this tag, we just arbitrarily limit to looking
     # at the last 100 submission.
-    recent_runs = ts.query(ts.Run).\
-        order_by(ts.Run.start_time.desc()).limit(100)
-    recent_runs = list(recent_runs)
+    recent_runs = ts.query(ts.Run) \
+        .join(ts.Order) \
+        .join(ts.Machine) \
+        .options(joinedload(ts.Run.order)) \
+        .options(joinedload(ts.Run.machine)) \
+        .order_by(ts.Run.start_time.desc()).limit(100)
+    recent_runs = recent_runs.all()
 
     # Compute the active machine list.
     active_machines = dict((run.machine.name, run)

jameseh96/cbnt

lnt/server/ui/views.py
@@ -21,6 +21,7 @@ from flask import render_template
 from flask import request, url_for
 from flask import session
 from flask_wtf import Form
+from sqlalchemy.orm import joinedload
 from sqlalchemy.orm.exc import NoResultFound
 from typing import List, Optional
 from wtforms import SelectField, StringField, SubmitField
@@ -189,12 +190,15 @@ def v4_recent_activity():
 
     # Get the most recent runs in this tag, we just arbitrarily limit to looking
     # at the last 100 submission.
-    recent_master_runs = ts.query(ts.Run). \
-        order_by(ts.Run.start_time.desc()).limit(100)
-    recent_master_runs = list(recent_master_runs)
-    recent_cv_runs = ts.query(ts.CVRun). \
-        order_by(ts.CVRun.start_time.desc()).limit(100)
-    recent_cv_runs = list(recent_cv_runs)
+
+    recent_master_runs = ts.query(ts.Run) \
+        .join(ts.Order) \
+        .join(ts.Machine) \
+        .options(joinedload(ts.Run.order)) \
+        .options(joinedload(ts.Run.machine)) \
+        .order_by(ts.Run.start_time.desc()).limit(100)
+    recent_master_runs = recent_master_runs.all()
+
     # Compute the active machine list.
     active_machines = dict((run.machine.name, run)
                            for run in recent_master_runs[::-1])
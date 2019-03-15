5447f294975f8d701a3ac96133b09011a8cfbc7d
Dave Rigby <daver@couchbase.com>
2019-03-15 09:53:52 +0000

    Fix missing .gitconfig issue
    Rebase to most recent server-ubuntu16-cv image - from the upstream change:
    
        repo sometimes complains if your git configuration doesn't have a
        username and email address. Bake .gitconfig into the image to
        avoid this.
    
    (See http://review.couchbase.org/#/c/106258/ for details).

---
+++ deployment/docker/client/1604/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+
+docker build -f Dockerfile -t cbnt_client ../../../../
+
+tag=$(date +%Y%m%d)
+echo "To tag and upload to Docker Hub (tagged with today's date) run:"
+echo ""
+echo "    docker login"
+echo "    docker tag cbnt_client:latest couchbaseeng/cbnt_client:${tag}"
+echo "    docker couchbaseeng/cbnt_client:${tag}"
